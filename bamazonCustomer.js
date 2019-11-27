c3a8a85ff940f352c80e26d161a2f36a0998486e
@xanderlav <alejandroregino.hussmann.com>
2019-11-26 18:38:23 -0600

    Set up DB connection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,14 @@
 // Declaring global variables
 var inquirer = require("inquirer");
+var mysql = require("mysql");
 
 // Defining DB configuration
-var host = 'localhost';
-var port = '3306';
+var connection = mysql.createConnection({
+    host:'localhost',
+    port: '3306',
+    user: "root",
+    password: "Xanderlav",
+    database: "bamazon"
+});
 
 
